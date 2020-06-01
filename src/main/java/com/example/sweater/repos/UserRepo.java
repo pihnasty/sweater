8568ece8c4a1b91fc2394739b5e1c322a768721d
POM <pihnastyi@gmail.com>
2020-06-01 23:49:03 +0300

    Spring Boot 03. Security

--- src/main/java/com/example/sweater/repos/UserRepo.java
+++ src/main/java/com/example/sweater/repos/UserRepo.java
@@ -1,10 +1,7 @@
 package com.example.sweater.repos;
 
-import com.example.sweater.domain.Message;
 import com.example.sweater.domain.User;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 
 public interface UserRepo extends JpaRepository<User, Long> {
