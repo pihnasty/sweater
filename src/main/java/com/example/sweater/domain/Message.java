08f9058dfa2328881fef21d4f1eb52f0adcb1e17
POM <pihnastyi@gmail.com>
2020-06-02 00:38:52 +0300

    Spring Boot 04. Add user in message

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -1,9 +1,7 @@
 package com.example.sweater.domain;
 
-import  javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -25,12 +23,21 @@ public class Message {
     private String tag;
     private String description = "description";
 
+    @ManyToOne(fetch = FetchType.EAGER)  // указываем, что хотим получать сообщение об авторе
+    @JoinColumn(name = "user_id")
+    private User author;
+
 //    public Message() {
 //    }
 
-    public Message(String text, String tag) {
+    public Message(String text, String tag, User user) {
         this.text = text;
         this.tag = tag;
+        this.author = user;
+    }
+
+    public String getAuthorName() {
+        return author !=null ? author.getUsername() : "<none>";
     }
 
 //    public Integer getId() {
